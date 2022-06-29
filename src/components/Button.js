37996c81fbb464d9f22c318997e7ca5c06231922
Tom Strong <strng_thms@yahoo.com>
2022-06-28 20:59:06 -0400

    Add background imgs and some styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,7 @@ import PropTypes from "prop-types";
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: color }}
-      className="btn"
-    >
+    <button onClick={onClick} style={{ color: color }} className="btn">
       {text}
     </button>
   );
